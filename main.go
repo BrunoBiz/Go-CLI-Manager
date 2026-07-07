595b750df226771376432eeb26f9dac9c934be1c
BrunoBiz <brunobizdc@gmail.com>
2026-07-06 23:36:37 -0300

    Server start exec

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"os/exec"
 )
 
 var firstParam string
@@ -11,11 +13,36 @@ func main() {
 	if len(os.Args) > 1 {
 		firstParam = os.Args[1]
 	} else {
-		firstParam = "--"
+		fmt.Println("ERR")
+		return
 	}
 
-	if firstParam == "START" {
-		fmt.Println("começou")
+	switch firstParam {
+	case "start":
+		//fmt.Println("COMEÇOU")
+		cmd := exec.Command("./ServerStart.sh")
+		cmd.Dir = "/opt/minecraft/MBC-Server/"
+
+		// 2. Connect the command's outputs directly to your terminal
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+
+		fmt.Println(cmd.Args)
+
+		// 3. Execute the script and wait for it to finish
+		err := cmd.Run()
+		if err != nil {
+			log.Fatalf("Script failed to run: %v", err)
+		}
+
+		fmt.Println("Script executed successfully!")
+
+	case "stop":
+		fmt.Println("PAROU")
+	case "restart":
+		fmt.Println("RESTART")
+	case "status":
+		fmt.Println("STATUS")
 	}
 
 	fmt.Println(firstParam)
