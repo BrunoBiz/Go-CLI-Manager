bd8f06b526d88d8caab5d3002ffdd79b09825eaf
BrunoBiz <brunobizdc@gmail.com>
2026-08-12 01:31:37 -0300

    New logger function no need for a struct

--- gameServerManager/logger/logger.go
+++ gameServerManager/logger/logger.go
@@ -2,62 +2,46 @@ package logger
 
 import (
 	"example/Go-CLI-Manager/gameServerManager/util"
+	"fmt"
 	"log/slog"
+	"os"
 	"os/exec"
+	"time"
 )
 
-type Logger struct {
-	logDir string
-}
-
-func NewLogger(config util.Config) *Logger {
-	logDir := config.GameServerDir + "/Log"
-
-	checkDirectory()
-
-	logger := &Logger{
-		logDir: logDir,
+func NewLogger(config util.Config) bool {
+	if checkDirectory() != nil {
+		return false
 	}
 
-	return logger
+	fileName := time.Now().Format(time.DateOnly)
+	logFile, err := os.OpenFile(fmt.Sprintf("Log/%s-gsm.log", fileName), os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
+
+	if err != nil {
+		slog.Error("Can't create log file - " + err.Error())
+		return false
+	}
+
+	logHandler := slog.NewJSONHandler(logFile, nil)
+	logger := slog.New(logHandler)
+	slog.SetDefault(logger)
+
+	return true
 }
 
-func checkDirectory() (bool, error) {
-	// if [ ! -d ./Log ]; then mkdir "Log" && echo "Created"; else echo "Already exists"; fi
-
-	//cmd := exec.Command(`/bin/bash`, `-c`, `"if [ ! -d /Log ]; then mkdir 'Log' && echo 'Created'; else echo 'Already exists'; fi"`)
-	cmd := exec.Command(`/bin/bash`, `-c`, `echo 'Hello from Go'`)
-
-	/*`if`,
-	`[`,
-	`!`,
-	`-d`,
-	`./Log`,
-	`]`,
-	`;`,
-	`then`,
-	`mkdir`,
-	`'Log'`,
-	`&&`,
-	`echo`,
-	`'Created'`,
-	`;`,
-	`else`,
-	`echo`,
-	`'Already exists'`,
-	`;`,
-	`fi`,
-	`"`)*/
+func checkDirectory() error {
+	cmd := exec.Command(`/bin/bash`, `-c`, `if [ ! -d Log ]; then mkdir 'Log' && echo 'Created'; else echo 'Already exists'; fi`) // TODO - There might be a better way to do this, but it works
 
 	tmux_sd, err := cmd.CombinedOutput()
-	slog.Info("LOGGER")
-	slog.Info(cmd.String())
+
+	slog.Debug("LOGGER")
+	slog.Debug(cmd.String())
 	slog.Info(string(tmux_sd))
 
 	if err != nil {
-		slog.Error(err.Error())
-		return false, err
+		slog.Error("Can't create log directory - " + err.Error())
+		return err
 	}
 
-	return false, nil
+	return nil
 }
