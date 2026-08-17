dee37bf873be148b97708fab24e395eca192e9ed
BrunoBiz <brunobizdc@gmail.com>
2026-08-16 21:22:41 -0300

    new log rotation function - delete any log file older than  15 days all log related functions were moved to a separate folder, leaves a cleaner look

---
+++ gameServerManager/logger/logger.go
@@ -0,0 +1,72 @@
+package logger
+
+import (
+	"example/Go-CLI-Manager/gameServerManager/util"
+	"fmt"
+	"log/slog"
+	"os"
+	"os/exec"
+	"time"
+)
+
+func LoadLogger(config util.Config) error {
+	//The 'Log' folder will always be in the root directory of GameServerManager
+	err := checkDirectory()
+	if err != nil {
+		return err
+	}
+
+	// Will delete any file older than 15 days
+	err = logRotation()
+	if err != nil {
+		return err
+	}
+
+	// Every log file will be named accordingly to the current date
+	fileName := time.Now().Format(time.DateOnly)
+	logFile, err := os.OpenFile(fmt.Sprintf("Log/%s-gsm.log", fileName), os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
+
+	if err != nil {
+		slog.Error("Can't create log file - " + err.Error())
+		return err
+	}
+
+	logger := slog.New(slog.NewJSONHandler(logFile, nil))
+	slog.SetDefault(logger)
+
+	return nil
+}
+
+func checkDirectory() error {
+	cmd := exec.Command(`/bin/bash`, `-c`, `if [ ! -d Log ]; then mkdir 'Log' && echo 'Created'; else echo 'Already exists'; fi`) // TODO - There might be a better way to do this, but it works
+	checkDir, err := cmd.CombinedOutput()
+
+	slog.Debug("checkDirectory()")
+	slog.Debug(cmd.String())
+	slog.Debug(string(checkDir))
+
+	// The return here (checkDir) does not matter, either it returns `Created` or  `Already exists`, both scenarios are acceptable, all that matters is that there was no errors
+	if err != nil {
+		slog.Error("Can't create log directory - " + err.Error())
+		return err
+	}
+
+	return nil
+}
+
+func logRotation() error {
+	// Will delete any log file older than 15 days
+	cmd := exec.Command(`/bin/bash`, `-c`, `find`, `./Log`, `-type`, `f`, `-mtime`, `+15`, `-exec`, `rm`, `{}`, `';'`)
+	deleteOldLogs, err := cmd.CombinedOutput()
+
+	slog.Debug("logRotation()")
+	slog.Debug(cmd.String())
+	slog.Debug(string(deleteOldLogs))
+
+	if err != nil {
+		slog.Error("Can't delete old logs - " + err.Error())
+		return err
+	}
+
+	return nil
+}
--- gameServerManager/main.go
+++ gameServerManager/main.go
@@ -2,12 +2,10 @@ package main
 
 import (
 	gameservermgr "example/Go-CLI-Manager/gameServerManager/gameServerMgr"
+	"example/Go-CLI-Manager/gameServerManager/logger"
 	"example/Go-CLI-Manager/gameServerManager/util"
-	"fmt"
 	"log/slog"
 	"os"
-	"os/exec"
-	"time"
 )
 
 func main() {
@@ -18,7 +16,7 @@ func main() {
 	}
 
 	// Create/Load the log file and set as the default logger for SLOG
-	err = LoadLogger(config)
+	err = logger.LoadLogger(config)
 	if err != nil {
 		slog.Error("ERR - Cannot load/create log file: " + err.Error())
 		return
@@ -31,42 +29,4 @@ func main() {
 		slog.Error("Main -> empty os.Args")
 		return
 	}
-}
-
-func LoadLogger(config util.Config) error {
-	//The 'Log' folder will always be in the root directory of GameServerManager
-	err := checkDirectory()
-	if err != nil {
-		return err
-	}
-
-	// Every log file will be named according to the current date
-	fileName := time.Now().Format(time.DateOnly)
-	logFile, err := os.OpenFile(fmt.Sprintf("Log/%s-gsm.log", fileName), os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
-
-	if err != nil {
-		slog.Error("Can't create log file - " + err.Error())
-		return err
-	}
-
-	logger := slog.New(slog.NewJSONHandler(logFile, nil))
-	slog.SetDefault(logger)
-
-	return nil
-}
-
-func checkDirectory() error {
-	cmd := exec.Command(`/bin/bash`, `-c`, `if [ ! -d Log ]; then mkdir 'Log' && echo 'Created'; else echo 'Already exists'; fi`) // TODO - There might be a better way to do this, but it works
-	tmux_sd, err := cmd.CombinedOutput()
-
-	slog.Debug("LOGGER")
-	slog.Debug(cmd.String())
-	slog.Debug(string(tmux_sd))
-
-	if err != nil {
-		slog.Error("Can't create log directory - " + err.Error())
-		return err
-	}
-
-	return nil
 }
